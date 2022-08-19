df5720e125a8671e60960672ad54858bff70680a
JonWong2020 <jonathannwong50@aol.com>
2022-08-18 22:47:19 -0700

    added numbers, spcchar, and letters

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// variables for characters, numbers and letters
+var specialChar = ["!", "U+0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}", "~"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+ 
   passwordText.value = password;
 
 }
