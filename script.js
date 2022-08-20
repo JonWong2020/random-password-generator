00e3cd576e2f5215d9458feb1f58f194d4796f40
JonWong2020 <jonathannwong50@aol.com>
2022-08-19 22:08:08 -0700

    added last for statements

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ function getRandomChar(list) {
 function generatePassword() {
   var userInput = window.prompt("How long would you like your password to be?");
 
-  var passwordLenth = parseInt(userInput)
+  var passwordLenth = parseInt(userInput);
 
   if (isNaN(passwordLenth)) {
-    window.alert("Please enter a number.")
-    return
+    window.alert("Please enter a number.");
+    return;
   }
 
   if (passwordLenth < 8 || passwordLenth > 128) {
-    window.alert("Password lenth must be between 8 and 128 characters")
-    return
+    window.alert("Password lenth must be between 8 and 128 characters");
+    return;
   }
 
 //confirmation statements 
   var userSymbols = window.confirm("Would you like speical characters? Ex. !, ?, /, etc.");
   var userLowercase = window.confirm("Would you like lowercase letters?");
   var userUppercase = window.confirm("Would you like UPPERCASE letters?");
-  var userNumbers = window.confirm("Would you like numbers?")
+  var userNumbers = window.confirm("Would you like numbers?");
  
 // variables for symbols, numbers and letters
   var symbols = ["!", "U+0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}", "~"];
@@ -37,26 +37,30 @@ function generatePassword() {
   var upperCase = [];
   var numbers = "0123456789".split("");
 
-  var criteriaArray = []
+  var criteriaArray = [];
 
   for (var i = 0; i < lowerCase.length; i++) {
-    upperCase[i] = lowerCase[i].toUpperCase()
+    upperCase[i] = lowerCase[i].toUpperCase();
   }
 
   if (userSymbols === true) {
-    criteriaArray.push(symbols)
+    criteriaArray.push(symbols);
   }
 
   if (userLowercase === true) {
-    criteriaArray.push(lowerCase)
+    criteriaArray.push(lowerCase);
   }
 
   if (userUppercase === true) {
-    criteriaArray.push(upperCase)
+    criteriaArray.push(upperCase);
   }
 
   if (userNumbers === true) {
-    criteriaArray.push(numbers)
+    criteriaArray.push(numbers);
+  }
+
+  if (criteriaArray.length === 0) {
+    criteriaArray.push(numbers);
   }
 
   var userPassword = ""
@@ -64,9 +68,10 @@ function generatePassword() {
   for (var i = 0; i <passwordLenth; i++) {
     var randomList = getRandomChar(criteriaArray);
     var randomChar = getRandomChar(randomList);
-    console.log(randomChar)
+    userPassword += randomChar;
   }
-
+  
+  return userPassword;
 }
 // Write password to the #password input
 function writePassword() {
